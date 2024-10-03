a6780a350c4db102e508392e52db5056d7630e2a
Ashrut panchal <ashrutpanchal111@gmail.com>
2024-10-03 17:39:00 +0530

    Perform crud operation and add comment

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,2 +1,7 @@
 class Article < ApplicationRecord
+  
+  has_many :comments, dependent: :destroy
+  validates :title, presence: true
+  validates :body, presence: true, length: { minimum: 12 }
+
 end
